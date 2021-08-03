f6a0798b3c6611e18a4d258a326f3be1ea6874a1
Daniil Khyzhniak <dkhyzhniak@brightgrove.com>
2021-08-03 00:16:56 -0700

    Add authentification endpoints

---
+++ CarsInfo.Infrastructure/MappersPL/RegisterVieModelMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CarsInfo.BLL.Models.Dtos;
+using CarsInfo.WebApi.ViewModels.ViewModels;
+
+namespace CarsInfo.Infrastructure.MappersPL
+{
+    public class RegisterVieModelMapperProfile : Profile
+    {
+        public RegisterVieModelMapperProfile()
+        {
+            CreateMap<RegisterViewModel, UserDto>();
+        }
+    }
+}
---
+++ CarsInfo.WebApi/Authorization/ApiAuthSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace CarsInfo.WebApi.Authorization
+{
+    public class ApiAuthSetting
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public int ExpirationTime { get; set; }
+    }
+}
