01228a9414b5ad64fd3168085d8d8f06194754f8
Daniil Khyzhniak <dkhyzhniak@brightgrove.com>
2021-08-03 00:14:54 -0700

    Add user repository

--- CarsInfo.DAL/DbContext.cs
+++ CarsInfo.DAL/DbContext.cs
@@ -34,6 +34,22 @@ namespace CarsInfo.DAL
             }
         }
 
+        public async Task<T> QueryFirstOrDefaultAsync<T, TFirst>(string sql, Func<T, TFirst, T> map, object parameters = null) 
+            where T : BaseEntity 
+            where TFirst : BaseEntity
+        {
+            try
+            {
+                var res = await _connection.QueryAsync(sql, map, parameters);
+                return res.FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred while fetching data");
+                return null;
+            }
+        }
+
         public async Task<T> QueryFirstOrDefaultAsync<T, TFirst, TSecond, TThird>(
             string sql, Func<T, TFirst, TSecond, TThird, T> map, object parameters = null)
             where T : BaseEntity
@@ -50,7 +66,7 @@ namespace CarsInfo.DAL
             {
                 _logger.LogError(e, "An error occurred while fetching data");
                 return null;
-            };
+            }
         }
 
         public async Task<IEnumerable<T>> QueryAsync<T>(string sql, object parameters = null) where T : BaseEntity
@@ -58,6 +74,21 @@ namespace CarsInfo.DAL
             try
             {
                 return await _connection.QueryAsync<T>(sql, parameters);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred while fetching data");
+                return new List<T>();
+            }
+        }
+
+        public async Task<IEnumerable<T>> QueryAsync<T, TFirst>(string sql, Func<T, TFirst, T> map, object parameters = null) 
+            where T : BaseEntity 
+            where TFirst : BaseEntity
+        {
+            try
+            {
+                return await _connection.QueryAsync(sql, map, parameters);
             }
             catch (Exception e)
             {
