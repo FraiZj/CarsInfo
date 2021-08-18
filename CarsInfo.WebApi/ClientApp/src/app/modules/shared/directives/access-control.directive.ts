2abab41fb29096b9a25e47ae8dbd001555896928
fra1z <dania.moom@gmail.com>
2021-08-18 14:46:52 +0300

    Add access-control directive

---
+++ CarsInfo.WebApi/ClientApp/src/app/modules/shared/directives/access-control.directive.ts
@@ -0,0 +1,41 @@
+import { UserClaims } from 'app/modules/auth/interfaces/user-claims';
+import { AuthService } from 'app/modules/auth/services/auth.service';
+import { Directive, ElementRef, Input, OnInit } from "@angular/core";
+
+@Directive({
+  selector: '[access-control]'
+})
+export class AccessControlDirective implements OnInit {
+  @Input() public roles: string[] = [];
+  @Input() public authenticated: boolean = true;
+  private defaultDisplay!: string;
+
+  constructor(
+    private elementRef: ElementRef,
+    private readonly authService: AuthService
+  ) { }
+
+  public ngOnInit(): void {
+    this.defaultDisplay = this.elementRef.nativeElement.style.display;
+    this.elementRef.nativeElement.style.display = 'none';
+    this.changeElementAppearance();
+  }
+
+  private changeElementAppearance(): void {
+    this.elementRef.nativeElement.style.display = this.hasAccess() ? this.defaultDisplay : 'none';
+  }
+
+  private hasAccess(): boolean {
+    const userClaims: UserClaims | null = this.authService.getCurrentUserClaims();
+
+    if (!this.authenticated) {
+      return userClaims == null;
+    }
+
+    if (this.authenticated && this.roles.length == 0) {
+      return userClaims != null;
+    }
+
+    return userClaims?.roles.some(r => this.roles.includes(r)) ?? false;
+  }
+}
--- CarsInfo.WebApi/ClientApp/src/app/modules/shared/shared.module.ts
+++ CarsInfo.WebApi/ClientApp/src/app/modules/shared/shared.module.ts
@@ -1,11 +1,19 @@
+import { AccessControlDirective } from './directives/access-control.directive';
+import { CommonModule } from '@angular/common';
+import { AuthModule } from './../auth/auth.module';
 import { NgModule } from "@angular/core";
 
 @NgModule({
   declarations: [
+    AccessControlDirective
   ],
   imports: [
+    CommonModule,
+
+    AuthModule
   ],
   exports: [
+    AccessControlDirective
   ]
 })
 export class SharedModule { }
