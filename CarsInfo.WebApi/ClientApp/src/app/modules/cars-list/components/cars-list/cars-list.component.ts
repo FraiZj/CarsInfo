079b4acabb7bff46f2b379fc62dfb4e4116786a3
fra1z <dania.moom@gmail.com>
2021-08-26 12:18:39 +0300

    Update filter service

--- CarsInfo.WebApi/ClientApp/src/app/modules/cars-list/components/cars-list/cars-list.component.ts
+++ CarsInfo.WebApi/ClientApp/src/app/modules/cars-list/components/cars-list/cars-list.component.ts
@@ -14,7 +14,8 @@ import { ItemsSkipPerLoad, ItemsTakePerLoad } from '../../consts/filter-consts';
 })
 export class CarsListComponent implements OnInit, OnDestroy {
   private readonly subscriptions: Subscription[] = [];
-  public filter!: FilterWithPaginator
+  private readonly filterName: string = 'cars-list-filter';
+  public filter!: FilterWithPaginator;
   public notEmptyPost = true;
   public notscrolly = true;
   public cars!: Car[];
@@ -26,10 +27,7 @@ export class CarsListComponent implements OnInit, OnDestroy {
   ) { }
 
   public ngOnInit(): void {
-    this.filter = this.filterService.getFilter() ?? {
-      skip: ItemsSkipPerLoad,
-      take: ItemsTakePerLoad
-    };
+    this.filter = this.filterService.getFilter(this.filterName) ?? FilterWithPaginator.CreateDefault();
     this.subscriptions.push(this.carsService.getCars(this.filter)
       .subscribe(cars => this.cars = cars));
   }
@@ -44,7 +42,7 @@ export class CarsListComponent implements OnInit, OnDestroy {
 
   public getFilteredCars(filter: FilterWithPaginator): void {
     this.filter = filter;
-    this.filterService.saveFilter(this.filter);
+    this.filterService.saveFilter(this.filterName, this.filter);
     this.subscriptions.push(this.carsService.getCars(this.filter)
       .subscribe(cars => this.cars = cars));
   }
@@ -59,7 +57,6 @@ export class CarsListComponent implements OnInit, OnDestroy {
 
   public loadNextCars(): void {
     this.filter.skip = this.cars.length;
-    this.filter.take = ItemsTakePerLoad;
     this.subscriptions.push(this.carsService.getCars(this.filter)
       .subscribe(cars => {
         this.spinner.hide();
