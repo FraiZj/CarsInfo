3bb4b80b57928662b462e5056aa85bb98fd4ebf5
fra1z <dania.moom@gmail.com>
2021-09-02 16:14:55 +0300

    Update auth dialog

--- CarsInfo.WebApi/ClientApp/src/app/modules/auth-dialog/auth-dialog.module.ts
+++ CarsInfo.WebApi/ClientApp/src/app/modules/auth-dialog/auth-dialog.module.ts
@@ -1,16 +1,17 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthModule } from '../auth/auth.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthDialogComponent } from './components/auth-dialog/auth-dialog.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatSnackBarModule,
 
     // app modules
-    AuthModule
+    AuthModule,
+    SharedModule
   ],
   exports: [
     AuthDialogComponent,
