3b976dc0fca4cc22a9f4294333199d7a44539490
fra1z <dania.moom@gmail.com>
2021-09-07 13:17:15 +0300

    Update connection string extension

--- CarsInfo.Infrastructure.DB/Extensions/SqlConnectionStringBuilderExtensions.cs
+++ CarsInfo.Infrastructure.DB/Extensions/SqlConnectionStringBuilderExtensions.cs
@@ -1,12 +1,18 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 
 namespace CarsInfo.Infrastructure.DB.Extensions
 {
     public static class SqlConnectionStringBuilderExtensions
     {
-        public static string GetDataSourceString(this SqlConnectionStringBuilder sqlConnectionStringBuilder)
+        private const string InitialCatalog = "Initial Catalog";
+        
+        public static string GetConnectionStringWithoutInitialCatalog(
+            this SqlConnectionStringBuilder sqlConnectionStringBuilder)
         {
-            return $"Data Source={sqlConnectionStringBuilder.DataSource};";
+            var copy = new SqlConnectionStringBuilder(sqlConnectionStringBuilder.ConnectionString);
+            copy.Remove(InitialCatalog);
+            return copy.ToString();
         }
     }
 }
