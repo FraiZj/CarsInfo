b3624d0fe92c6dd8a95d5b8699f578cee7ea444f
fra1z <dania.moom@gmail.com>
2021-09-08 16:23:58 +0300

    Add oper result to brand service

--- CarsInfo.WebApi/Controllers/Base/AppControllerBase.cs
+++ CarsInfo.WebApi/Controllers/Base/AppControllerBase.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using CarsInfo.Application.BusinessLogic.OperationResult;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarsInfo.WebApi.Controllers.Base
 {
@@ -14,6 +16,48 @@ namespace CarsInfo.WebApi.Controllers.Base
         public virtual BadRequestObjectResult ApplicationError(string error)
         {
             return BadRequest(error);
+        }
+
+        protected virtual IActionResult PutResult(
+            OperationResultBase operation,
+            Func<IActionResult> onSuccess)
+        {
+            return PutAndDeleteResult(operation, onSuccess);
+        }
+
+        protected virtual IActionResult DeleteResult(
+            OperationResultBase operation,
+            Func<IActionResult> onSuccess)
+        {
+            return PutAndDeleteResult(operation, onSuccess);
+        }
+        
+        private IActionResult PutAndDeleteResult(
+            OperationResultBase operation, 
+            Func<IActionResult> onSuccess)
+        {
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ? 
+                onSuccess() :
+                BadRequest(operation.FailureMessage);
+        }
+        
+        public virtual IActionResult GetResult<T>(
+            OperationResult<T> operation, 
+            Func<T, IActionResult> onSuccess)
+        {
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ? 
+                onSuccess(operation.Result) :
+                BadRequest(operation.FailureMessage);
         }
     }
 }
--- CarsInfo.WebApi/Controllers/BrandController.cs
+++ CarsInfo.WebApi/Controllers/BrandController.cs
@@ -2,10 +2,12 @@
 using System.Threading.Tasks;
 using CarsInfo.Application.BusinessLogic.Contracts;
 using CarsInfo.Application.BusinessLogic.Dtos;
+using CarsInfo.Application.BusinessLogic.Enums;
 using CarsInfo.WebApi.Controllers.Base;
 using CarsInfo.WebApi.Mappers;
 using CarsInfo.WebApi.ViewModels;
 using CarsInfo.WebApi.ViewModels.Brand;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarsInfo.WebApi.Controllers
@@ -23,40 +25,81 @@ namespace CarsInfo.WebApi.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<BrandDto>> Get(string name)
+        public async Task<IActionResult> Get(string name)
         {
-            var brands = await _brandService.GetAllAsync(name);
-            return brands;
+            var operation = await _brandService.GetAllAsync(name);
+            
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ? 
+                Ok(operation.Result) :
+                BadRequest(operation.FailureMessage);
         }
 
         [HttpGet("{id:int}")]
-        public async Task<BrandDto> Get(int id)
+        public async Task<ActionResult<BrandDto>> Get(int id)
         {
-            var brand = await _brandService.GetByIdAsync(id);
-            return brand;
+            var operation = await _brandService.GetByIdAsync(id);
+            
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ?
+                Ok(operation.Result) :
+                BadRequest(operation.FailureMessage);
         }
 
-        [HttpPost]
+        [HttpPost, Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> Create([FromBody] BrandEditorViewModel brand)
         {
             var brandDto = _mapper.MapToBrandDto(brand);
-            await _brandService.AddAsync(brandDto);
-            return Created("", brand);
+            var operation = await _brandService.AddAsync(brandDto);
+
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ?
+                CreatedAtAction(nameof(Get), new { id = operation.Result }, brand) :
+                BadRequest(operation.FailureMessage);
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Update([FromBody] BrandEditorViewModel brand)
+        [HttpPut("{id:int}"), Authorize(Roles = Roles.Admin)]
+        public async Task<IActionResult> Update(int id, [FromBody] BrandEditorViewModel brand)
         {
             var brandDto = _mapper.MapToBrandDto(brand);
-            await _brandService.UpdateAsync(brandDto);
-            return Ok(brand);
+            brandDto.Id = id;
+            var operation = await _brandService.UpdateAsync(brandDto);
+            
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ?
+                NoContent() :
+                BadRequest(operation.FailureMessage);
         }
 
-        [HttpDelete("{id:int}")]
+        [HttpDelete("{id:int}"), Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> Delete(int id)
         {
-            await _brandService.DeleteByIdAsync(id);
-            return Ok($"Brand with id={id} has been deleted");
+            var operation = await _brandService.DeleteByIdAsync(id);
+            
+            if (operation.IsException)
+            {
+                return ApplicationError();
+            }
+            
+            return operation.Success ?
+                NoContent() :
+                BadRequest(operation.FailureMessage);
         }
     }
 }
