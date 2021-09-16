48df2f85d24fa581eacb2edd5ee2586e0097a566
fra1z <dania.moom@gmail.com>
2021-09-16 16:32:28 +0300

    Add google auth service

---
+++ CarsInfo.Infrastructure.BusinessLogic/External/Auth/Google/GoogleAuthService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarsInfo.Application.BusinessLogic.External.Auth.Google;
+using CarsInfo.Application.BusinessLogic.External.Auth.Google.Models;
+using CarsInfo.Application.BusinessLogic.OperationResult;
+using Google.Apis.Auth;
+using Microsoft.Extensions.Logging;
+
+namespace CarsInfo.Infrastructure.BusinessLogic.External.Auth.Google
+{
+    public class GoogleAuthService : IGoogleAuthService
+    {
+        private readonly GoogleAuthSettings _googleAuthSettings;
+        private readonly ILogger<GoogleAuthService> _logger;
+
+        public GoogleAuthService(
+            GoogleAuthSettings googleAuthSettings, 
+            ILogger<GoogleAuthService> logger)
+        {
+            _googleAuthSettings = googleAuthSettings;
+            _logger = logger;
+        }
+
+        public async Task<OperationResult<GoogleAuthResult>> AuthenticateAsync(string accessToken)
+        {
+            try
+            {
+                var payload = await GoogleJsonWebSignature.ValidateAsync(accessToken, 
+                    new GoogleJsonWebSignature.ValidationSettings
+                    {
+                        Audience = new List<string>
+                        {
+                            _googleAuthSettings.ClientId
+                        }
+                    });
+
+                var result = new GoogleAuthResult
+                {
+                    Email = payload.Email,
+                    FirstName = payload.GivenName,
+                    LastName = payload.FamilyName
+                };
+
+                return OperationResult<GoogleAuthResult>.SuccessResult(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred while authentication thought google api");
+                return OperationResult<GoogleAuthResult>.ExceptionResult();
+            }
+        }
+    }
+}
---
+++ CarsInfo.WebApi/StartupConfiguration/GoogleAuthConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using CarsInfo.Application.BusinessLogic.External.Auth.Google.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarsInfo.WebApi.StartupConfiguration
+{
+    public static class GoogleAuthConfiguration
+    {
+        public static void AddGoogleAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            var googleAuthSettings = new GoogleAuthSettings();
+            configuration.GetSection(nameof(GoogleAuthSettings)).Bind(googleAuthSettings);
+            services.AddSingleton(googleAuthSettings);
+        }
+    }
+}
