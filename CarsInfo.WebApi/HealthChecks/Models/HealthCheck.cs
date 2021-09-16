12944f977e4b2aedef755ee1ac1bc1e359fd19f6
fra1z <dania.moom@gmail.com>
2021-09-16 12:04:38 +0300

    Add health check models

---
+++ CarsInfo.WebApi/HealthChecks/Models/HealthCheck.cs
@@ -0,0 +1,11 @@
+﻿namespace CarsInfo.WebApi.HealthChecks.Models
+{
+    public class HealthCheck
+    {
+        public string Component { get; set; }
+
+        public string Status { get; set; }
+
+        public string Description { get; set; }
+    }
+}
