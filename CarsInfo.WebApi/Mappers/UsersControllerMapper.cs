902e61d34eb7db49519c6be99bd12f9a558bfd04
fra1z <dania.moom@gmail.com>
2021-09-29 11:09:34 +0300

    Add users controller

---
+++ CarsInfo.WebApi/Mappers/UsersControllerMapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CarsInfo.Application.BusinessLogic.Dtos;
+using CarsInfo.WebApi.ViewModels.Users;
+
+namespace CarsInfo.WebApi.Mappers
+{
+    public class UsersControllerMapper
+    {
+        public UserReadViewModel MapToUserReadViewModel(UserDto userDto)
+        {
+            if (userDto is null)
+            {
+                return null;
+            }
+
+            return new UserReadViewModel
+            {
+                Id = userDto.Id,
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                Email = userDto.Email,
+                EmailVerified = userDto.EmailVerified,
+                Roles = userDto.Roles
+            };
+        }
+
+        public IEnumerable<UserReadViewModel> MapToUserReadViewModel(IEnumerable<UserDto> userDtos)
+        {
+            return userDtos.Select(MapToUserReadViewModel);
+        }
+        
+        public UserEditorDto MapToUserEditorDto(UserEditorViewModel model)
+        {
+            if (model is null)
+            {
+                return null;
+            }
+
+            return new UserEditorDto
+            {
+                Roles = model.Roles
+            };
+        }
+    }
+}
---
+++ CarsInfo.WebApi/ViewModels/Users/UserReadViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CarsInfo.WebApi.ViewModels.Users
+{
+    public class UserReadViewModel
+    {
+        public int Id { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        
+        public bool EmailVerified { get; set; }
+
+        public ICollection<string> Roles { get; set; }
+    }
+}
